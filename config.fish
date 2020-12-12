01d7d67dd8e2d0025c1893b17edf216130a21600
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2020-12-12 14:48:20 +0530

    Add fish config.

---
+++ config.fish
@@ -0,0 +1,23 @@
+# Aliases
+
+## Shell aliases
+alias ll "ls -l"
+alias lal "ls -al"
+
+## Git aliases
+alias ga "git add"
+alias gc "git commit --untracked-files=no"
+alias gch "git checkout"
+alias gcp "git cherry-pick"
+alias gf "git fetch"
+alias gl "git log"
+alias gpr "git pull --rebase"
+alias gpsh "git push"
+alias gpshf "git push --force"
+alias grb "git rebase"
+alias grm "git remote"
+alias grs "git reset"
+alias gs "git status --untracked-files=no"
+alias gsu "git status"
+alias gsh "git show"
+alias gshf "git show --pretty="" --name-only"
