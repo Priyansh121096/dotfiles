594d9b7234881f3755d7dc5fb9806a565a1e14b4
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2020-12-24 01:14:02 +0530

    Add shorthand for sourcing default fish config to the fish config.
    - Add a couple more git aliases.
    - Change the alias for git remote to grmt to avoid confusion with
      git rm.

--- config.fish
+++ config.fish
@@ -4,18 +4,23 @@
 alias ll "ls -l"
 alias lal "ls -al"
 
+## Source my default fish config
+alias srcfish "source /home/agrawpri/.config/fish/config.fish"
+
 ## Git aliases
 alias ga "git add"
 alias gc "git commit --untracked-files=no"
 alias gch "git checkout"
 alias gcp "git cherry-pick"
+alias gd "git diff"
+alias gdc "git diff --cached"
 alias gf "git fetch"
 alias gl "git log"
 alias gpr "git pull --rebase"
 alias gpsh "git push"
 alias gpshf "git push --force"
 alias grb "git rebase"
-alias grm "git remote"
+alias grmt "git remote"
 alias grs "git reset"
 alias gs "git status --untracked-files=no"
 alias gsu "git status"
